1003b236fc7a20d37c2a078c13a0fad843e43833
Nasir Uddin <nasiruddin8476@gmail.com>
2024-10-09 16:05:06 +0600

    Your commit message here

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,12 +1,45 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgIf],
   templateUrl: './admin.component.html',
-  styleUrl: './admin.component.css'
+  styleUrls: ['./admin.component.css']
 })
 export class AdminComponent {
 
+  model: any = {};
+  cover!: string;
+  showError = false;
+
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = (event) => {
+        const dataUrl = reader.result!.toString();
+        this.cover = dataUrl; 
+      };
+
+      reader.readAsDataURL(file); 
+      this.showError = false;
+    }
+  }
+
+  onSubmit(form: NgForm) {
+    if (form.invalid || !this.cover) {
+      form.control.markAllAsTouched();
+      if(!this.cover){
+        this.showError = true;
+      }
+      return;
+    }
+    console.log(form.value);
+  }
 }
